1122cdba0a2768461d9ca3f9a67a7a9ff4cb82e1
Kylbot8 <147377080+Kylbot8@users.noreply.github.com>
2023-10-11 10:14:08 +0200

    Add files via upload

---
+++ exercice 6/exo6.js
@@ -0,0 +1,8 @@
+let a = 3;
+let b = 4;
+
+function addition(number1, number2) {
+  console.log(number1 + number2);
+}
+
+addition(a, b);
---
+++ exercice 7/exo7.js
@@ -0,0 +1,9 @@
+let name = ["Riri", "Fifi", "Loulou"];
+console.log(name);  
+name.push("Picsou", "Donald");
+console.log(name);
+name.splice(1, 1);
+console.log(name);
+console.log(name.push());
+name.splice(1, 0, "Kylian");
+console.log(name);
